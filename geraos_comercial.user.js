c6723fc16e1401d4d53a9fd72bebdebb31c93508
ferreiraturbo <139795557+ferreiraturbo@users.noreply.github.com>
2023-09-01 15:05:50 -0300

    Update geraos_comercial.user.js
    Nova versão com atualização

--- geraos_comercial.user.js
+++ geraos_comercial.user.js
@@ -5,8 +5,8 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://erp.turbonettelecom.com.br/dashboard_seller
-// @downloadURL  https://raw.githubusercontent.com/ferreiraturbo/geraos/master/geraos.user.js
-// @updateURL    https://raw.githubusercontent.com/ferreiraturbo/geraos/master/geraos.user.js
+// @downloadURL  https://github.com/ferreiraturbo/geraos_comercial/raw/main/geraos_comercial.user.js
+// @updateURL    https://github.com/ferreiraturbo/geraos_comercial/raw/main/geraos_comercial.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=turbonettelecom.com.br
 // @grant        none
 // ==/UserScript==
